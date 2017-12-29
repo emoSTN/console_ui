1e035c3cfd57b29cb4b5211254e6d8bf47aa9052
Emil Zdravkov <emil.zdravkov@markovskisolutions.com>
2017-12-29 19:45:30 +0200

    Decorations test

--- user_input/main.py
+++ user_input/main.py
@@ -1,4 +1,5 @@
 import getpass
+import re
 import sys
 
 from user_input import validations
@@ -25,6 +26,51 @@ def user_input(label, validators):
             return inp
 
 
+class NotBlank(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        while True:
+            res = self.func(*args, **kwargs)
+            if not res or res is None or re.match('/\A\s*\z/', res):
+                print('Invalid input')
+            else:
+                break
+
+
+class Size(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, func):
+        print "Inside __call__()"
+
+        def wrapped_f(*args, **kwargs):
+            print("Inside wrapped_f()")
+            print("Decorator arguments:", self.size)
+            func(*args, **kwargs)
+            print("After f(*args)")
+
+        return wrapped_f
+
+
+@NotBlank
+def d_user_input(label):
+    while True:
+        inp = input(label)
+        check = True
+        if not validators:
+            return inp
+        for validator in validators:
+            check = check and validator.call_func(inp)
+            if not check:
+                print('Invalid input')
+                break
+        if check:
+            return inp
+
+
 def pass_input():
     label = 'Enter password: '
     while True:
@@ -35,3 +81,11 @@ def pass_input():
         if validations.not_blank(inp):
             return inp
         print('Password must not be empty')
+
+
+def main():
+    print(user_input('Test input', validators))
+
+
+if __name__ == '__main__':
+    main()
